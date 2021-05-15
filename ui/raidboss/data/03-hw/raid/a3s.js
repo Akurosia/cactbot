e5ef80c65fe4447fe0af6af34bcfeada3b383199
Akurosia <kamothegamer@web.de>
2021-05-15 22:16:24 +0200

    some updates

--- ui/raidboss/data/03-hw/raid/a3s.js
+++ ui/raidboss/data/03-hw/raid/a3s.js
@@ -346,7 +346,7 @@ export default {
         'Fluid Swing': 'Flüssiger Schwung',
         'Gear Lubricant': 'Getriebeschmiermittel',
         'Hand Of Pain': 'Qualhand',
-        'Hand Of Prayer': 'Betende Hand',
+        'Hand Of Prayer/Parting': 'Betende/Scheidende Hand',
         'Hydromorph': 'Hydromorphose',
         'Magnetism': 'Magnetismus',
         'Piston Lubricant': 'Kolbenschmiermittel',
